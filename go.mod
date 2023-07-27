0d9f5b0dd879eaa7fafbaeb6566b7370902c168b
aya5899 <qwertmmnbv@gmail.com>
2023-07-27 12:58:59 +0900

    add article_test.go

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/aya5899/goapi
 go 1.17
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
---
+++ repositories/articles_test.go
@@ -0,0 +1,52 @@
+package repositories_test
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	"github.com/aya5899/goapi/models"
+	"github.com/aya5899/goapi/repositories"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestSelectArticleDetail(t *testing.T) {
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	// databaseに接続
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	expected := models.Article{
+		ID:       1,
+		Title:    "firstPost",
+		Contents: "This is my first blog",
+		UserName: "mayah",
+		NiceNum:  3,
+	}
+	got, err := repositories.SelectArticleDetail(db, expected.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got.ID != expected.ID {
+		t.Errorf("get %d but want %d\n", got.ID, expected.ID)
+	}
+	if got.Title != expected.Title {
+		t.Errorf("get %s but want %s\n", got.Title, expected.Title)
+	}
+	if got.Contents != expected.Contents {
+		t.Errorf("get %s but want %s\n", got.Contents, expected.Contents)
+	}
+	if got.UserName != expected.UserName {
+		t.Errorf("get %s but want %s\n", got.UserName, expected.UserName)
+	}
+	if got.NiceNum != expected.NiceNum {
+		t.Errorf("get %d but want %d\n", got.NiceNum, expected.NiceNum)
+	}
+}
