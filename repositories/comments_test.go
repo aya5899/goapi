32f255bebf4b2f7d39f1ac598f432575fbf5da33
aya5899 <qwertmmnbv@gmail.com>
2023-07-27 22:24:34 +0900

    add TestInsertComment

--- repositories/comments_test.go
+++ repositories/comments_test.go
@@ -3,6 +3,7 @@ package repositories_test
 import (
 	"testing"
 
+	"github.com/aya5899/goapi/models"
 	"github.com/aya5899/goapi/repositories"
 	"github.com/aya5899/goapi/repositories/testdata"
 )
@@ -16,4 +17,28 @@ func TestSelectCommentList(t *testing.T) {
 	if commentNum := len(got); commentNum != expectedCommentNum {
 		t.Errorf("want %d but got %d articles \n", expectedCommentNum, commentNum)
 	}
+}
+
+func TestInsertComment(t *testing.T) {
+	comment := models.Comment{
+		ArticleID: 1,
+		Message:   "comment insertion test",
+	}
+	expectedCommentID := 3
+	newComment, err := repositories.InsertComment(testDB, comment)
+	if err != nil {
+		t.Error(err)
+	}
+	if newComment.CommentID != expectedCommentID {
+		t.Errorf("new comment id is expected %d but got %d\n", expectedCommentID, newComment.CommentID)
+
+	}
+	// clean up
+	t.Cleanup(func() {
+		const sqlStr = `
+		delete from comments
+		where message = ?;
+		`
+		testDB.Exec(sqlStr, comment.Message)
+	})
 }
